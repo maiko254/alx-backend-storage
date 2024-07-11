058ade57e1c7fa5bfb92cb1653eaa9469149355b
maiko254 <bonyomichael406.com>
2024-07-11 13:26:41 +0300

    mysql script creating a trigger that decreases quantity attribute in table items when a new row is inserted into table orders

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,8 @@
+DELIMITER $$
+CREATE TRIGGER update_quantity
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items SET quantity = (quantity - NEW.number) WHERE name = NEW.item_name;
+END
+$$
