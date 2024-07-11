d41d5586c43b3ec4cf16f628ffab4925a1a9ece3
maiko254 <bonyomichael406.com>
2024-07-11 15:48:17 +0300

    Script creating a stored procedured that calculates and stores the average score for a student

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,20 @@
+-- Script creating a stored procedured that calculates and stores the average score for a student
+
+DELIMITER //
+
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+BEGIN
+	DECLARE total_score INT;
+	DECLARE total_projects INT;
+	DECLARE avg_score FLOAT;
+
+	SELECT SUM(score) INTO total_score FROM corrections WHERE user_id = user_id;
+
+	SELECT COUNT(*) INTO total_projects FROM corrections WHERE user_id = user_id;
+
+	SET avg_score = total_score / total_projects;
+
+	UPDATE users SET average_score = avg_score WHERE id = user_id;
+END //
+
+DELIMITER ;
