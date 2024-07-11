6ddfa355d90aa8405abe8940b752b0491017f46f
maiko254 <bonyomichael406.com>
2024-07-11 09:33:53 +0300

    mysql script creating table users with columns id, email, name and setting attribute email to be unique

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,10 @@
+-- mysql script creating table users with the following attributes
+-- id, integer, never null, auto increment and primary key
+-- email, string (255 characters), never null and unique
+-- name, string (255 characters)
+
+CREATE TABLE IF NOT EXISTS users (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255)
+);
