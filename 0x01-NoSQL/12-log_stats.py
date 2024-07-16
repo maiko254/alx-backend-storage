875e3a759abe34cbe6fb19c969e5b4c16d4c6c20
maiko254 <bonyomichael406.com>
2024-07-16 11:23:28 +0300

    Correction: Ensuring script is not run when imported

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -3,15 +3,16 @@
 from pymongo import MongoClient
 
 
-client = MongoClient()
-db = client.logs
-coll = db.nginx
+if __name__ == "__main__":
+    client = MongoClient()
+    db = client.logs
+    coll = db.nginx
 
-print(f"{coll.count_documents({})} logs \nMethods:")
-print(f"    method GET: {coll.count_documents({'method': 'GET'})}")
-print(f"    method POST: {coll.count_documents({'method': 'POST'})}")
-print(f"    method PUT: {coll.count_documents({'method': 'PUT'})}")
-print(f"    method PATCH: {coll.count_documents({'method': 'PATCH'})}")
-print(f"    method DELETE: {coll.count_documents({'method': 'DELETE'})}")
-filter = {'method': 'GET', 'path': '/status'}
-print(f"{coll.count_documents(filter)} status check")
+    print(f"{coll.count_documents({})} logs \nMethods:")
+    print(f"    method GET: {coll.count_documents({'method': 'GET'})}")
+    print(f"    method POST: {coll.count_documents({'method': 'POST'})}")
+    print(f"    method PUT: {coll.count_documents({'method': 'PUT'})}")
+    print(f"    method PATCH: {coll.count_documents({'method': 'PATCH'})}")
+    print(f"    method DELETE: {coll.count_documents({'method': 'DELETE'})}")
+    filter = {'method': 'GET', 'path': '/status'}
+    print(f"{coll.count_documents(filter)} status check")
