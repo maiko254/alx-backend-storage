793b9b5f0b900d7080bf53dbf14eeb42dd4fed7a
maiko254 <bonyomichael406.com>
2024-07-16 11:20:59 +0300

    feat: Script using pymongo to filter nginx logs stored in MongoDB

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Module filtering nginx logs in MongoDB"""
+from pymongo import MongoClient
+
+
+client = MongoClient()
+db = client.logs
+coll = db.nginx
+
+print(f"{coll.count_documents({})} logs \nMethods:")
+print(f"    method GET: {coll.count_documents({'method': 'GET'})}")
+print(f"    method POST: {coll.count_documents({'method': 'POST'})}")
+print(f"    method PUT: {coll.count_documents({'method': 'PUT'})}")
+print(f"    method PATCH: {coll.count_documents({'method': 'PATCH'})}")
+print(f"    method DELETE: {coll.count_documents({'method': 'DELETE'})}")
+filter = {'method': 'GET', 'path': '/status'}
+print(f"{coll.count_documents(filter)} status check")
