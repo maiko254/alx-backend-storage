8947dc0d79a2a6fa281e6d7eda4117f4493ecccb
maiko254 <bonyomichael406.com>
2024-07-17 13:48:05 +0300

    feat: Get data from redis and apply an optional function that converts the data to the required format

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """Module implementing a redis cache"""
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -18,3 +18,21 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str,
+            fn: Callable = None) -> Union[str, bytes, int, float]:
+        """Get data from Redis and apply an optional function."""
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """Returns string data from redis"""
+        return self._redis.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: int) -> int:
+        """Returns integer data from redis"""
+        return self._redis.get(key, fn=int)
